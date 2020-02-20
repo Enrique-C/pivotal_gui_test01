c7730bd057975e03a4952a11a08138f2bae8b2bb
Enrique-C <Enrique.Carrizales@fundacion-jala.org>
2020-02-20 10:52:46 -0400

    Last TCs

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -1,4 +1,5 @@
 import Core.WebDriverManager;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
@@ -114,7 +115,15 @@ public class Calculator {
         WebDriverManager.getInstance().quitDriver();
     }
     @Test
-    public void Division_55By55_1() {
+    public void Multiplication_0p000000000000001By999999999_0p000000999999999() {
+        CalculatorPage calculatorPage = new CalculatorPage();
+        String actualResult = calculatorPage.run("0.000000000000001x999999999=");
+        String expectedResult = "0.000000999999999";
+        assertEquals(expectedResult, actualResult);
+        WebDriverManager.getInstance().quitDriver();
+    }
+    @Test
+    public void Division_55Between55_1() {
         CalculatorPage calculatorPage = new CalculatorPage();
         String actualResult = calculatorPage.run("55/55=");
         String expectedResult = "1";
@@ -122,10 +131,58 @@ public class Calculator {
         WebDriverManager.getInstance().quitDriver();
     }
     @Test
-    public void Division_0By9999_0() {
+    public void Division_999Between0_Error() {
+        CalculatorPage calculatorPage = new CalculatorPage();
+        String actualResult = calculatorPage.run("999/0=");
+        String expectedResult = "Infinity";
+        assertEquals(expectedResult, actualResult);
+        WebDriverManager.getInstance().quitDriver();
+    }
+    @Test
+    public void Division_0p1Between555_1p8018018018018018018018018018018en4() {
+        CalculatorPage calculatorPage = new CalculatorPage();
+        String actualResult = calculatorPage.run("0.1/555=");
+        String expectedResult = "1.8018018018018018018018018018018e-4";
+        assertEquals(expectedResult, actualResult);
+        WebDriverManager.getInstance().quitDriver();
+    }
+    @Test
+    public void Division_0Between9999_0() {
         CalculatorPage calculatorPage = new CalculatorPage();
         String actualResult = calculatorPage.run("0/9999=");
         String expectedResult = "0";
+        assertEquals(expectedResult, actualResult);
+        WebDriverManager.getInstance().quitDriver();
+    }
+    @Test
+    public void AdditionAndSubtraction_2plus2minus2_2() {
+        CalculatorPage calculatorPage = new CalculatorPage();
+        String actualResult = calculatorPage.run("2+2-2=");
+        String expectedResult = "2";
+        assertEquals(expectedResult, actualResult);
+        WebDriverManager.getInstance().quitDriver();
+    }
+    @Test
+    public void SubtractionAndMultiplication_2minus2By99_n196() {
+        CalculatorPage calculatorPage = new CalculatorPage();
+        String actualResult = calculatorPage.run("2-2x99=");
+        String expectedResult = "-196";
+        assertEquals(expectedResult, actualResult);
+        WebDriverManager.getInstance().quitDriver();
+    }
+    @Test
+    public void SubtractionAndMultiplicationAndDivision_2minus2By80Between2_n78() {
+        CalculatorPage calculatorPage = new CalculatorPage();
+        String actualResult = calculatorPage.run("2-2x80/2=");
+        String expectedResult = "-78";
+        assertEquals(expectedResult, actualResult);
+        WebDriverManager.getInstance().quitDriver();
+    }
+    @Test
+    public void SubtractionAndMultiplicationAndDivision_2minus2By0Between2_2() {
+        CalculatorPage calculatorPage = new CalculatorPage();
+        String actualResult = calculatorPage.run("2-2x0/2=");
+        String expectedResult = "2";
         assertEquals(expectedResult, actualResult);
         WebDriverManager.getInstance().quitDriver();
     }
